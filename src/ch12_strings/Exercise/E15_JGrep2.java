3a55e6d97acc1818ba7f78fdcb25d79a838c4d33
Jienan Zhang <zjn0505@hotmail.com>
2017-03-23 21:24:30 +0800

    Finish Regex

---
+++ src/ch12_strings/Exercise/E15_JGrep2.java
@@ -0,0 +1,53 @@
+package ch12_strings.Exercise;
+
+/**
+ * Created by jienanzhang on 23/03/2017.
+ */
+// {Args: src\ch12_strings\Exercise\E15_JGrep2.java "\b[Ssct]\w+" CASE_INSENSITIVE}
+
+import net.mindview.util.TextFile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*********************** Exercise 15 *********************
+ * Modify JGrep.java to accept flags as arguments (e.g.,
+ * Pattern.CASE_INSENSITIVE, Pattern.MULTILINE).
+ *********************************************************/
+public class E15_JGrep2 {
+    public static void main(String[] args) throws Exception {
+        if (args.length < 3) {
+            System.out.println("Usage: java E15_JGrep2 filesout regex pattern ");
+            System.out.println("pattern can take one of the following values");
+            System.out.println("CANON_EQ, CASE_INSENSITIVE, COMMENTS, " +
+                "DOTALL, MULTILINE, UNICODE_CASE, UNIX_LINES");
+            System.exit(0);
+        }
+        int flag = 0;
+        if (args[2].equalsIgnoreCase("CANON_EQ")) {
+            flag = Pattern.CANON_EQ;
+        } else if (args[2].equalsIgnoreCase("CASE_INSENSITIVE")) {
+            flag = Pattern.CASE_INSENSITIVE;
+        } else if (args[2].equalsIgnoreCase("COMMENTS")) {
+            flag = Pattern.COMMENTS;
+        } else if (args[2].equalsIgnoreCase("DOTALL")) {
+            flag = Pattern.DOTALL;
+        } else if (args[2].equalsIgnoreCase("MULTILINE")) {
+            flag = Pattern.MULTILINE;
+        } else if (args[2].equalsIgnoreCase("UNICODE_CASE")) {
+            flag = Pattern.UNICODE_CASE;
+        } else if (args[2].equalsIgnoreCase("UNIX_LINES")) {
+            flag = Pattern.UNIX_LINES;
+        }
+        Pattern p = Pattern.compile(args[1], flag);
+        // Iterate through the lines of the input file:
+        int index = 0;
+        Matcher m = p.matcher("");
+        for (String line : new TextFile(args[0])) {
+            m.reset(line);
+            while (m.find())
+                System.out.println(index++ + ": " + m.group() + ": " +
+                    m.start());
+        }
+    }
+}
---
+++ src/ch12_strings/Exercise/E18_JStringExtractor.java
@@ -0,0 +1,38 @@
+package ch12_strings.Exercise;
+
+/**
+ * Created by jienanzhang on 23/03/2017.
+ */
+// {Args: src\ch12_strings\Exercise\E18_JStringExtractor.java}
+
+import net.mindview.util.TextFile;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/********************** Exercise 18 *********************
+ * Write a program that reads a Java source-code file
+ * and displays all the string literals in the code
+ * (provide the file name on the command line).
+ ********************************************************/
+public class E18_JStringExtractor {
+    static final String STR_EXT_REGEX =
+            "\"(?:[^\"\\\\\\n\\r]|(?:\\\\(?:[untbrf\\\\'\"]"
+                    + "|[0-9A-Fa-f]{4}|[0-7]{1,2}|[0-3][0-7]{2})))*\"";
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println(
+                    "Usage: java E18_JStringExtractor file");
+            System.exit(0);
+        }
+        String src = TextFile.read(args[0]);
+        Pattern p =
+                Pattern.compile(STR_EXT_REGEX);
+        Matcher m = p.matcher(src);
+        while (m.find())
+            System.out.println(m.group().
+                    substring(1, m.group().length() - 1));
+        // "This is NOT a string but a comment!"
+        String dummy = "\u003F\u003f\n\060\607";
+    }
+}
